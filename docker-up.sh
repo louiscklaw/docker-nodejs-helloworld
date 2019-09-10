0f37a8be62ec5925ff2c3972cfd8c88bca0fdfd7
louiscklaw <louiscklaw@gmail.com>
2019-09-10 20:08:39 +0800

    update,

--- docker-up.sh
+++ docker-up.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+git pull
+
+echo 'docker down'
+docker-compose kill && docker-compose down
+
+echo 'docker up'
 docker-compose up -d --force-recreate --build --remove-orphans
 
 docker-compose ps
